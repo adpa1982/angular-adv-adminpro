7527f28076b3508f1886e6ac17c7c54962679085
adpa1982 <adpa363@hotmail.com>
2020-08-31 22:55:34 -0500

    Fin seccion 19 Lazy Load y despliegue

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanLoad, Route, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -9,10 +9,21 @@ import { UsuarioService } from './../services/usuario.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private usuarioService: UsuarioService,
               private router: Router) { }
+
+  canLoad(route: Route, segments: UrlSegment[]): boolean  {
+    return this.usuarioService.validarToken()
+                .pipe(
+                  tap( estaAutenticado => {
+                      if ( !estaAutenticado ){
+                         this.router.navigateByUrl('/login');
+                      }
+                  })
+                );
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,54 +2,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AuthGuard } from './../guards/auth.guard';
-import { AdminGuard } from './../guards/admin.guard';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProgressComponent } from './progress/progress.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
-import { PromesasComponent } from './promesas/promesas.component';
-import { RxjsComponent } from './rxjs/rxjs.component';
-import { PerfilComponent } from './perfil/perfil.component';
-import { BusquedaComponent } from './busqueda/busqueda.component';
-
-// Mantenimiento
-import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
-import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
-import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
-import { MedicoComponent } from './mantenimientos/medicos/medico.component';
 
 const routes: Routes = [
   { path: 'dashboard',
     component: PagesComponent,
     canActivate: [AuthGuard],
-    children: [
-      { path: '', component: DashboardComponent, data: { titulo: 'Dashboard' } },
-      { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
-      { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica #1' } },
-      { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Account-Settings' } },
-      { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
-      { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs' } },
-      { path: 'perfil', component: PerfilComponent, data: { titulo: 'Perfil de usuario' } },
-      { path: 'buscar/:termino', component: BusquedaComponent, data: { titulo: 'Busquedas' } },
-
-      // Mantenimientos
-      { path: 'hospitales', component: HospitalesComponent, data: { titulo: 'Mantenimiento de Hospitales' } },
-      { path: 'medicos', component: MedicosComponent, data: { titulo: 'Mantenimiento de Medicos' } },
-      { path: 'medico/:id', component: MedicoComponent, data: { titulo: 'Mantenimiento de Medico' } },
-
-      // Rutas Admin
-      { path: 'usuarios', canActivate: [AdminGuard], component: UsuariosComponent, data: { titulo: 'Mantenimiento de Usuarios' } },
-    ]
+    canLoad: [AuthGuard],
+    loadChildren: () => import('./child-routes.module').then( m => m.ChildRoutesModule)
   },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forChild( routes )
-  ],
+  imports: [ RouterModule.forChild( routes ) ],
   exports: [ RouterModule ]
 })
 export class PagesRoutingModule { }
